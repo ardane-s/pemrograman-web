ca7af5bf81f244f0067b974294590e8ec0c374c5
Ardane Machieous <komangaris2910@gmail.com>
2024-04-18 10:06:20 +0800

    feat: Update

--- ardanePhp/index.php
+++ ardanePhp/index.php
@@ -1,4 +1,5 @@
 <?php 
+    /*
     session_start();
     include_once('prak1.php');
     echo "<hr>";
@@ -34,4 +35,37 @@
     echo "<hr>";
     include_once('function2.php');
     echo "<hr>";
+    */
  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ardane PHP</title>
+</head>
+<body>
+
+<h1>PRAKTEK PHP - PEMROGRAMAN WEB</h1>
+
+<a href="prak1.php">Praktek 1</a><br>
+<a href="prak2.php">Praktek 2</a><br>
+<a href="prak3.php">Praktek 3</a><br>
+<a href="kondisi1.php">Kondisi 1</a><br>
+<a href="kondisi2.php">Kondisi 2</a><br>
+<a href="kondisi3.php">Kondisi 3</a><br>
+<a href="perulangan1.php">Perulangan 1</a><br>
+<a href="perulangan2.php">Perulangan 2</a><br>
+<a href="perulangan3.php">Perulangan 3</a><br>
+<a href="perulangan4.php">Perulangan 4</a><br>
+<a href="perulangan5.php">Perulangan 5</a><br>
+<a href="perulangan6.php">Perulangan 6</a><br>
+<a href="array1.php">Array 1</a><br>
+<a href="array2.php">Array 2</a><br>
+<a href="array3.php">Array 3</a><br>
+<a href="function1.php">Function 1</a><br>
+<a href="function2.php">Function 2</a><br>
+
+</body>
+</html>
